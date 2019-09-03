9136100b34f17f12812c20864a98cc4c8e147483
Chris Müller <chris@krue.ml>
2019-09-03 21:07:56 +0200

    [TASK] Remove unneeded call_user_func()

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,11 +1,8 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-call_user_func(function()
-{
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        'extpagetitle',
-        'Configuration/TypoScript',
-        'Extension Page Title'
-    );
-});
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'extpagetitle',
+    'Configuration/TypoScript',
+    'Extension Page Title'
+);
