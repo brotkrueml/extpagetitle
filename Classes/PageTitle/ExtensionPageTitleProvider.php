c6c6f22745ef31e348f0a9be6cacb2679eb3a570
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2019-11-16 23:43:05 +0100

    [TASK] Use declare(strict_types=1) without spaces to be compatible with PSR-12

--- Classes/PageTitle/ExtensionPageTitleProvider.php
+++ Classes/PageTitle/ExtensionPageTitleProvider.php
@@ -1,7 +1,14 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Brotkrueml\Extpagetitle\PageTitle;
+
+/*
+ * This file is part of the "extpagetitle" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
 
 use TYPO3\CMS\Core\PageTitle\AbstractPageTitleProvider;
 
--- Tests/Unit/PageTitle/ExtensionPageTitleProviderTest.php
+++ Tests/Unit/PageTitle/ExtensionPageTitleProviderTest.php
@@ -1,10 +1,12 @@
 <?php
+declare(strict_types=1);
 
 namespace Brotkrueml\Extpagetitle\Tests\Unit\PageTitle;
 
 use Brotkrueml\Extpagetitle\PageTitle\ExtensionPageTitleProvider;
+use PHPUnit\Framework\TestCase;
 
-class ExtensionPageTitleProviderTest extends \PHPUnit\Framework\TestCase
+class ExtensionPageTitleProviderTest extends TestCase
 {
     public function testSetTitle(): void
     {
