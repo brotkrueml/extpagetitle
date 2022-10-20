08cb9669ecd7370dbe59784cb28c04aa68b9cba2
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-10-20 19:13:54 +0200

    [TASK] Update coding-standards to version 2.0.2

--- ecs.php
+++ ecs.php
@@ -2,10 +2,9 @@
 
 declare (strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (ECSConfig $config): void {
     $header = <<<HEADER
 This file is part of the "extpagetitle" extension for TYPO3 CMS.
 
@@ -13,22 +12,15 @@ For the full copyright and license information, please read the
 LICENSE file that was distributed with this source code.
 HEADER;
 
-    $containerConfigurator->import(__DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php');
+    $config->import(__DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php');
 
-    $services = $containerConfigurator->services();
-    $services->set(\PhpCsFixer\Fixer\Comment\HeaderCommentFixer::class)
-        ->call('configure', [[
-            'comment_type' => 'comment',
-            'header' => $header,
-            'separate' => 'both',
-        ]]);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(
-        Option::PATHS,
-        [
-            __DIR__ . '/Classes',
-            __DIR__ . '/Tests',
-        ]
-    );
+    $config->paths([
+        __DIR__ . '/Classes',
+        __DIR__ . '/Tests',
+    ]);
+    $config->ruleWithConfiguration(\PhpCsFixer\Fixer\Comment\HeaderCommentFixer::class, [
+        'comment_type' => 'comment',
+        'header' => $header,
+        'separate' => 'both',
+    ]);
 };
