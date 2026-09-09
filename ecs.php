c50436152d9d259183fe576882b0bcb79b4f83e8
Chris Müller <coding@brotkrueml.dev>
2026-09-09 18:15:48 +0200

    Update coding-standards to 6.3.1

--- ecs.php
+++ ecs.php
@@ -2,25 +2,30 @@
 
 declare (strict_types=1);
 
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ECSConfig $config): void {
-    $header = <<<HEADER
+$header = <<<HEADER
 This file is part of the "extpagetitle" extension for TYPO3 CMS.
 
 For the full copyright and license information, please read the
 LICENSE file that was distributed with this source code.
 HEADER;
 
-    $config->import(__DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php');
-
-    $config->paths([
+return ECSConfig::configure()
+    ->withSets([
+        __DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php',
+    ])
+    ->withParallel()
+    ->withPaths([
         __DIR__ . '/Classes',
         __DIR__ . '/Tests',
-    ]);
-    $config->ruleWithConfiguration(\PhpCsFixer\Fixer\Comment\HeaderCommentFixer::class, [
-        'comment_type' => 'comment',
-        'header' => $header,
-        'separate' => 'both',
-    ]);
-};
+    ])
+    ->withConfiguredRule(
+        HeaderCommentFixer::class,
+        [
+            'comment_type' => 'comment',
+            'header' => $header,
+            'separate' => 'both',
+        ],
+    );
